ae80c4f39654dbc892a45c47b9d0fd428ccda5be
Joel Negron <joeljnegron@gmail.com>
2020-12-07 10:42:15 -0500

    Create pollutant function R file complete part 1 in Markdown

---
+++ pollutantmean.R
@@ -0,0 +1,16 @@
+
+# R Programming Week 2 Assignment -----------------------------------------
+
+# Pollutant Mean Function:
+pullutantmean <- function(directory, pollutant, id = 1:332) {
+        # Create a list with the data based on the id arguement.
+        monitorList <- lapply(id, function(id) {
+                padId <- str_pad(as.character(id), width = 3, side = "left", pad = "0")
+                path <- paste0(directory, "/", padId, '.csv')
+                return(read.csv(path))
+        })
+        # rbind the data frames and estimate the mean
+        monitorData <- do.call("rbind", monitorList)
+        pollutantAvg <- mean(monitorData[, pollutant], na.rm = TRUE)
+        return(pollutantAvg)
+}
